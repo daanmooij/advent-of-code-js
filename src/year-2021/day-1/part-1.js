9be604a81209335c70caf9ab2c4095972e041984
Daan Mooij <daanmooij@gmail.com>
2021-12-02 23:34:34 +0100

    add year 2021 day 1 part 1 solution

---
+++ src/year-2021/day-1/part-1.js
@@ -0,0 +1,15 @@
+import * as util from 'util';
+import * as fs from 'fs';
+
+const readFileAsync = util.promisify(fs.readFile);
+
+const input = await readFileAsync('input.txt', 'utf-8');
+const measurements = input.split('\n').map(n => +n);
+
+let counter = 0;
+measurements.forEach((value, index, array) => {
+    if (index === 0 ) { return; }
+    if (value > array[index-1]) { counter++; }
+});
+
+console.log(counter);
