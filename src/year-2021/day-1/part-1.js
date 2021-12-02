f84293a54fc94191266c0e0df603409c2489dfb6
Daan Mooij <daanmooij@gmail.com>
2021-12-03 00:18:04 +0100

    format some lines

--- src/year-2021/day-1/part-1.js
+++ src/year-2021/day-1/part-1.js
@@ -8,8 +8,8 @@ const measurements = input.split('\n').map(n => +n);
 
 let counter = 0;
 measurements.forEach((value, index, array) => {
-    if (index === 0 ) { return; }
-    if (value > array[index-1]) { counter++; }
+    if (index === 0) return;
+    if (value > array[index - 1]) counter++;
 });
 
 console.log(counter);
