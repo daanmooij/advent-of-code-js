5a50eb9b938be9b6b39e7134836413662b3b91d1
Daan Mooij <daanmooij@gmail.com>
2023-12-05 18:12:16 +0100

    refactor 2023 day 1 part 1

--- src/2023/1/solution.js
+++ src/2023/1/solution.js
@@ -2,18 +2,20 @@ const fs = require('fs')
 const path = require('path')
 
 const filePath = path.join(__dirname, './input.txt')
-const input = fs.readFileSync(filePath, 'utf-8')
+const lines = fs.readFileSync(filePath, 'utf-8').trim().split('\n')
 
-const part1 = input
-  .split('\n')
-  .map((line) =>
-    line
-      .split('')
-      .filter((n) => /^\d$/.test(n))
-      .filter((_, i, a) => i === 0 || i === a.length - 1)
-      .map((n, i, a) => (a.length === 1 ? n.repeat(2) : n))
-      .join(''),
-  )
-  .reduce((sum, n) => sum + Number(n), 0)
+const digits = lines.map((line) =>
+  line.split('').filter((char) => /^\d$/.test(char)),
+)
 
-console.log(part1)
+const calibrations = digits.map((digits) => {
+  const firstDigit = digits.shift()
+  return firstDigit + (digits.pop() ?? firstDigit)
+})
+
+const sum = calibrations.reduce(
+  (sum, calibration) => sum + Number(calibration),
+  0,
+)
+
+console.log(sum)
