0c922b3c7bbbaed9caa7ab350c8982f5fc7b375b
Daan Mooij <daanmooij@gmail.com>
2023-12-05 22:06:26 +0100

    add 2023 day 1 part 2

--- src/2023/1/solution.js
+++ src/2023/1/solution.js
@@ -18,4 +18,51 @@ const sum = calibrations.reduce(
   0,
 )
 
-console.log(sum)
+console.log(`Part 1: ${sum}`)
+
+const letterDigits = new Map([
+  ['one', 1],
+  ['two', 2],
+  ['three', 3],
+  ['four', 4],
+  ['five', 5],
+  ['six', 6],
+  ['seven', 7],
+  ['eight', 8],
+  ['nine', 9],
+])
+
+const totalSum = lines.reduce((sum, line) => {
+  let highestIndex = -1
+  let lowestIndex = line.length
+  let firstDigit = 0
+  let lastDigit = 0
+
+  letterDigits.forEach((digit, word) => {
+    const firstWordIndex = line.indexOf(word)
+    const lastWordIndex = line.lastIndexOf(word)
+    const firstDigitIndex = line.indexOf(digit.toString())
+    const lastDigitIndex = line.lastIndexOf(digit.toString())
+    const maxIndex = Math.max(lastWordIndex, lastDigitIndex)
+    const minIndex = Math.min(
+      ...[firstWordIndex, firstDigitIndex, lowestIndex].filter(
+        (index) => index > -1,
+      ),
+    )
+
+    if (highestIndex < maxIndex) {
+      highestIndex = maxIndex
+      lastDigit = digit
+    }
+
+    if (lowestIndex > minIndex) {
+      lowestIndex = minIndex
+      firstDigit = digit
+    }
+  })
+
+  const calibration = Number(`${firstDigit}${lastDigit}`)
+  return sum + calibration
+}, 0)
+
+console.log(`Part 2: ${totalSum}`)
